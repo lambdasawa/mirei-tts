2c7bb2e58fb14ad69a4d4b908ca08faac2cd185f
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-03-10 10:01:43 +0900

    :sparkles: Add script that generate dictionary

--- Makefile
+++ Makefile
@@ -4,7 +4,10 @@ dev:
 	go run application.go
 
 deploy:
-	git add -f voice/* text-seed.json
+	git add -f voice/* text-seed.json ipa.dic
 	eb setenv GOPATH='/tmp/go'
 	eb deploy --staged
-	git restore --staged voice/* text-seed.json
+	git restore --staged voice/* text-seed.json ipa.dic
+
+generate-dictionary:
+	./bin/generate-dictionary.sh
---
+++ bin/generate-dictionary.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -xeu
+
+pushd "$(pwd)"
+
+mkdir -p tmp/dic
+cd tmp/dic
+
+# fetch mecab dictionary
+# http://taku910.github.io/mecab/
+curl -sSL -o mecab-ipadic.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM"
+tar zxf mecab-ipadic.tar.gz
+
+# fetch neologd
+# https://github.com/neologd/mecab-ipadic-neologd
+curl -sSL -o mecab-user-dict-seed.csv.xz https://github.com/neologd/mecab-ipadic-neologd/raw/master/seed/mecab-user-dict-seed.20200130.csv.xz
+unxz mecab-user-dict-seed.csv.xz
+
+# fetch kagome tools
+# https://ikawaha.hateblo.jp/entry/2016/04/20/145940
+git clone https://github.com/ikawaha/kagome
+go run kagome/cmd/_dictool/main.go ipa -mecab mecab-ipadic-2.7.0-20070801/ -neologd mecab-user-dict-seed.csv
+
+popd
+
+mv tmp/dic/ipa.dic ./
+
+rm -rf tmp/dic
